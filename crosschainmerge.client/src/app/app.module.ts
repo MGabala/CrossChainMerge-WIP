98e0fecc5d12d4912fa0dd952221133a2086ee7a
Mateusz <82496006+MGabala@users.noreply.github.com>
2024-03-17 14:29:14 +0100

    Dodanie głównych komponentów

--- crosschainmerge.client/src/app/app.module.ts
+++ crosschainmerge.client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransferComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
